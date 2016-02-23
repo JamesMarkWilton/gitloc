53f817e19fd71c8c112a2cdfebd22f59ef052e70
James Wilton <james.mark.wilton@gmail.com>
2016-02-23 09:55:30 -0600

    Moved code into a lib directory

---
+++ lib/gitloc.rb
@@ -0,0 +1,23 @@
+require 'tmpdir'
+require 'open3'
+
+class Gitloc
+  def self.call(argv, outstream, errstream)
+    repo = ARGV.first
+
+    Dir.mktmpdir { |dir|
+      Dir.chdir dir
+      out, err, status = Open3.capture3 'git', 'clone', repo, 'cloned'
+      unless status.success?
+        $stderr.puts out, err
+        exit "Failed somehow >.<"
+      end
+      Dir.chdir 'cloned'
+      files = Dir['**/*'].reject { |name| File.directory? name }
+      files.each do |filename|
+        gitloc = File.readlines(filename).count { |line| line !~ /^\s*$/ }
+        puts "#{gitloc}\t#{filename}"
+      end
+    }
+  end
+end
